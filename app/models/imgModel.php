087d21600043a32f9ff4d1f0a6d3cb0056eb3cb7
ptho1504 <phuoctho150420@gmail.com>
2023-12-14 15:23:30 +0700

    Fixed Image

--- app/models/imgModel.php
+++ app/models/imgModel.php
@@ -10,7 +10,7 @@ class ImgModel
 
     public function getAll()
     {
-        $query = "SELECT * FROM tour_image join tour on tour_image.tour_id = tour.tour_id ORDER BY RAND() LIMIT 20 ";
+        $query = "SELECT DISTINCT * FROM tour_image join tour on tour_image.tour_id = tour.tour_id ORDER BY RAND() LIMIT 20 ";
         $data = $this->database->select($query);
         return $data->fetch_all(MYSQLI_ASSOC);
     }
