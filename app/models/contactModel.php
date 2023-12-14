d256899380acfa402b9a4485d6bd2d0f89248bc0
mtriSE <mtri0162@gmail.com>
2023-12-15 03:03:47 +0700

    thêm quản lý thông tin liên hệ khách hàng

---
+++ app/models/contactModel.php
@@ -0,0 +1,18 @@
+<?php
+
+class ContactModel {
+    private $database;
+    public function __construct()
+    {
+        $this->database = new Database();
+    }
+
+    public function getAll()
+    {
+        $query = "SELECT * FROM contact";
+        $data = $this->database->select($query);
+        return $data->fetch_all(MYSQLI_ASSOC);
+    }
+
+    
+}
