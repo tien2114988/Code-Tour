6b93e77c43f4e8e16ff69f9c7eb99cacb0919645
Thanh Trung <trandacthanhtrungvd@gmail.com>
2023-12-14 22:45:49 +0700

    Add Comment Response features

--- public/js/main.js
+++ public/js/main.js
@@ -8,4 +8,14 @@ $(document).ready(function() {
     $('.usr-confirm-btn').click((e) => {
         window.location.replace(userDel);
     })
+
+    // Handle delete comment (feedback)
+    var cmtDel;
+    $('.cmt-delete-btn').click((e) => {
+        cmtDel = e.target.href;
+        e.preventDefault();
+    });
+    $('.cmt-confirm-btn').click((e) => {
+        window.location.replace(cmtDel);
+    })
 });
