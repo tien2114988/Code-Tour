ee33232482fced2cc73f7f3b7126673086a0f9f9
Thanh Trung <trandacthanhtrungvd@gmail.com>
2023-12-14 10:58:17 +0700

    Contact features

---
+++ app/views/admin/tabs/contact/success.php
@@ -0,0 +1,8 @@
+<div class="flex w-full h-[80vh] justify-center items-center">
+    <div class="flex flex-col justify-between items-center max-w-md h-44 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Cập nhật thành công</h5>
+        <a href="<?php echo $path; ?>admin/contact" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Quay lại
+        </a>
+    </div>
+</div>
