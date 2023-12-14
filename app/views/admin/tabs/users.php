e7a4737429f1c6184fb28a6a496735916b1978f8
Thanh Trung <trandacthanhtrungvd@gmail.com>
2023-12-14 11:06:26 +0700

    Remove dark mode

--- app/views/admin/tabs/users.php
+++ app/views/admin/tabs/users.php
@@ -1,18 +1,18 @@
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <div class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900">
+    <div class="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white">
         <button>Thêm</button>
         <label for="table-search" class="sr-only">Search</label>
         <div class="relative">
             <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                 </svg>
             </div>
-            <input type="text" id="table-search-users" class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for users">
+            <input type="text" id="table-search-users" class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Search for users">
         </div>
     </div>
-    <table class="w-full text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-lg text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <table class="w-full text-left rtl:text-right text-gray-500">
+        <thead class="text-lg text-gray-700 bg-gray-50">
             <tr>
                 <th scope="col" class="px-6 py-3">
                     Tên
@@ -28,8 +28,8 @@
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+            <tr class="bg-white border-b hover:bg-gray-50">
+                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
                     <img class="w-10 h-10 rounded-full" src="https://picsum.photos/128/128" alt="avatar">
                     <div class="ps-3">
                         <div class="text-base font-semibold">Thanh Trung</div>
@@ -48,8 +48,8 @@
                     <button class="py-2 px-4 font-medium text-red-400 border border-red-400 rounded-lg hover:text-white hover:bg-red-400 mr-2">Xóa</button>
                 </td>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+            <tr class="bg-white border-b hover:bg-gray-50">
+                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
                     <img class="w-10 h-10 rounded-full" src="https://picsum.photos/128/128" alt="avatar">
                     <div class="ps-3">
                         <div class="text-base font-semibold">Thanh Trung</div>
@@ -68,8 +68,8 @@
                     <button class="py-2 px-4 font-medium text-red-400 border border-red-400 rounded-lg hover:text-white hover:bg-red-400 mr-2">Xóa</button>
                 </td>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+            <tr class="bg-white border-b hover:bg-gray-50">
+                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
                     <img class="w-10 h-10 rounded-full" src="https://picsum.photos/128/128" alt="avatar">
                     <div class="ps-3">
                         <div class="text-base font-semibold">Thanh Trung</div>
@@ -88,8 +88,8 @@
                     <button class="py-2 px-4 font-medium text-red-400 border border-red-400 rounded-lg hover:text-white hover:bg-red-400 mr-2">Xóa</button>
                 </td>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+            <tr class="bg-white border-b hover:bg-gray-50">
+                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
                     <img class="w-10 h-10 rounded-full" src="https://picsum.photos/128/128" alt="avatar">
                     <div class="ps-3">
                         <div class="text-base font-semibold">Thanh Trung</div>
@@ -108,8 +108,8 @@
                     <button class="py-2 px-4 font-medium text-red-400 border border-red-400 rounded-lg hover:text-white hover:bg-red-400 mr-2">Xóa</button>
                 </td>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+            <tr class="bg-white border-b hover:bg-gray-50">
+                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
                     <img class="w-10 h-10 rounded-full" src="https://picsum.photos/128/128" alt="avatar">
                     <div class="ps-3">
                         <div class="text-base font-semibold">Thanh Trung</div>
@@ -128,8 +128,8 @@
                     <button class="py-2 px-4 font-medium text-red-400 border border-red-400 rounded-lg hover:text-white hover:bg-red-400 mr-2">Xóa</button>
                 </td>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+            <tr class="bg-white border-b hover:bg-gray-50">
+                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
                     <img class="w-10 h-10 rounded-full" src="https://picsum.photos/128/128" alt="avatar">
                     <div class="ps-3">
                         <div class="text-base font-semibold">Thanh Trung</div>
@@ -148,8 +148,8 @@
                     <button class="py-2 px-4 font-medium text-red-400 border border-red-400 rounded-lg hover:text-white hover:bg-red-400 mr-2">Xóa</button>
                 </td>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+            <tr class="bg-white border-b hover:bg-gray-50">
+                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
                     <img class="w-10 h-10 rounded-full" src="https://picsum.photos/128/128" alt="avatar">
                     <div class="ps-3">
                         <div class="text-base font-semibold">Thanh Trung</div>
@@ -168,8 +168,8 @@
                     <button class="py-2 px-4 font-medium text-red-400 border border-red-400 rounded-lg hover:text-white hover:bg-red-400 mr-2">Xóa</button>
                 </td>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+            <tr class="bg-white border-b hover:bg-gray-50">
+                <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap">
                     <img class="w-10 h-10 rounded-full" src="https://picsum.photos/128/128" alt="avatar">
                     <div class="ps-3">
                         <div class="text-base font-semibold">Thanh Trung</div>
